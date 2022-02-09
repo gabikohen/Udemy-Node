c693c2423f5f273c5a8facbb1b54dfbee3f69c6f
gabikohen <gabokohen@gmail.com>
2022-02-09 19:02:31 -0300

    mapbox

--- 06-clima-app/index.js
+++ 06-clima-app/index.js
@@ -1,35 +1,46 @@
 require("colors");
 
 
-const {
-  inquererMenu,leerInput,pausa
-  
-} = require("./helpers/inquerer");
+const { inquererMenu, leerInput, pausa } = require("./helpers/inquerer");
 const Busquedas = require("./models/busquedas");
 
-
 const main = async () => {
+  const busquedas = new Busquedas();
 
-    const busquedas = new Busquedas();
-    
   let opt;
- 
 
   do {
     //Imprimir el menu
     opt = await inquererMenu();
 
     switch (opt) {
-        case 1:
-             // Mostrar mensaje
-            break;
-    
-        default:
-            break;
+      case 1:
+        // Mostrar mensaje
+
+        const lugar = await leerInput('Ciudad:')
+       await busquedas.ciudad(lugar)
+        // Buscar el lugar
+        // Selectcionar el lugar
+
+        //clima
+
+        //resultado
+
+        console.log("\nInformacion de la ciudad\n");
+        console.log("Ciudad:");
+        console.log("Lat:");
+        console.log("Lng:");
+        console.log("Temperatura:",);
+        console.log("Minima:",);
+        console.log("Maxima:",);
+        break;
+
+      default:
+        break;
     }
-    console.log({opt});
-    
-   if(opt !== 0 ) await pausa();
+    console.log({ opt });
+
+    if (opt !== 0) await pausa();
   } while (opt !== 0);
 };
 
--- 06-clima-app/models/busquedas.js
+++ 06-clima-app/models/busquedas.js
@@ -1,17 +1,36 @@
+const axios = require("axios").default;
+
 class Busquedas {
-  historial = ["Madrid", "New York", "Paris", "Buenos Aires"];
+  historial = ["Londres", "New york", "Paris", "Buenos aires"];
 
   constructor() {
     //Leer DB si existe
   }
+
+  get paramsMapBox() {
+    return {
+      "acces_token":
+        "pk.eyJ1IjoiZ2FpYmtvaGVuIiwiYSI6ImNremZ0ZG5rZjBpY3Eyd216ZmoxaDNxMDYifQ.UOI1RQ0quRO-okZ6C65WLg",
+     " limit": 5,
+      "language": "es",
+    };
+  }
+
   async ciudad(lugar = "") {
-    console.log(lugar);
+    // Peticiones Http
+    try {
+      const intance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapBox,
+      });
+      const resp = await intance.get();
 
-
-   return [];  // retornar las ciudades que coincidad con 
-}
-
-
+      console.log(resp.data);
+      return []; 
+    } catch (error) {
+      return []; // retornar las ciudades que coincidad con
+    }
+  }
 }
 
 module.exports = Busquedas;
