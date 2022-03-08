d70478d6d03cb5d2c2f5909375d5db39ffb82647
gabikohen <gabokohen@gmail.com>
2022-03-08 14:16:01 -0300

    arreglando objeto

--- 08.restserver/app.js
+++ 08.restserver/app.js
@@ -1,3 +1,15 @@
+/* require('dotenv').config(); */
+
+/* const express = require('express');
+const app = express(); */
+
+/* app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+ */
+/* app.listen(process.env.PORT, ()=>{
+    console.log('Servidor corriendo en el puerto',process.env.PORT)
+}) */
 
 
 require('dotenv').config();
--- 08.restserver/models/server.js
+++ 08.restserver/models/server.js
@@ -3,29 +3,50 @@ const express = require("express");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
 
     // Middlewares
-     this.middlewares();
-    // Rutas de mi aplicacion 
+    this.middlewares();
+
+    // Rutas Aplicacion
     this.routes();
   }
 
-  middlewares(){
-    // Directorio Publico
-    this.app.use(express.static('public'));
-
+  middlewares() {
+    // Directorio publico
+    this.app.use(express.static("public"));
   }
 
   routes() {
-    this.app.get("/", (req, res) => {
-      res.send("Hello World");
+    this.app.get("/api", (req, res) => {
+      res.json({
+        msg: "get API",
+      });
+    });
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put API",
+      });
+    });
+    this.app.post("/api", (req, res) => {
+      res.json({
+        msg: "post API",
+      });
+    });
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete API",
+      });
+    });
+    this.app.patch("/api", (req, res) => {
+      res.json({
+        msg: "patch API",
+      });
     });
   }
 
   listen() {
-    this.app.listen(this.port ,() => {
-      console.log("Servidor corriendo", this.port);
+    this.app.listen(process.env.PORT, () => {
+      console.log("Servidor corriendo en el puerto", process.env.PORT);
     });
   }
 }
