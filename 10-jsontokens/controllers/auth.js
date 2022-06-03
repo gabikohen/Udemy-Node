706a269fb3104866e393dfb12b75f65f8afaeeab
gabikohen <gabokohen@gmail.com>
2022-06-02 21:19:00 -0300

    JWT

--- 10-jsontokens/controllers/auth.js
+++ 10-jsontokens/controllers/auth.js
@@ -1,13 +1,39 @@
-const {response} = require('express')
+const { response } = require("express");
 
-const login = (req,res= response) =>{
+const Usuario = require("../models/usuario");
 
-    res.json({
-        msg:'Login ok'
-    })
+/* const  */
 
-}
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    // Verificar si el email existe
+
+    const user = await Usuario.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        msg: "User | Pass no son correctos - EMAIL",
+      });
+    }
+
+    // Si el usuario esta activo
+    // Verificar la contrasena
+    //Generar el JWT
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Hable con el ADM",
+    });
+  }
+
+  res.json({
+    msg: "Login ok",
+    email,
+    password,
+  });
+};
 
 module.exports = {
-    login
-}
+  login,
+};
--- 10-jsontokens/routes/auth.js
+++ 10-jsontokens/routes/auth.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 const {login} = require('../controllers/auth')
+const validarCamp = require('../middlewares/validarCampos')
+const {check} = require('express-validator');
 
 const router = Router();
 
-router.post("/login",login);
+router.post("/login",[check('email','El email es obligatorio').isEmail(),check('password','El password es obligatorio').notEmpty(),validarCamp],login);
 
 
 module.exports = router;
